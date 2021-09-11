49833b2712bd3556b7dc0854fc465728f1a3a0f8
Dima Sotskiy <sotskiydr@gmail.com>
2021-09-12 02:08:34 +0300

    ready

---
+++ script.js
@@ -0,0 +1,22 @@
+const slider = document.querySelector('.slider');
+let counter = 1;
+function switcher (){
+    slider.innerHTML = `<img src="/pictures/Joker-${counter}.jpg" alt="Joker-${counter}">`;
+}
+const prevBtn = document.querySelector('.prev');
+prevBtn.addEventListener('click' , () => {
+    if(counter <= 1){
+        return
+    }
+    counter--
+    switcher()
+})
+const nextBtn = document.querySelector('.next');
+nextBtn.addEventListener('click' , () => {
+    if(counter >= 9){
+        return
+    }
+    counter++
+    switcher()
+})
+switcher()
